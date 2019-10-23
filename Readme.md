44cdb704263238b6fe1f474b851a22556d77ee18
Fahad Ashraf <fahad@Fahads-MBP.fios-router.home>
2019-10-23 00:44:50 -0400

    Added readme, cleaned up code

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Getting Started #
+
+##Follow the following guide to install GPIO Libraries:##
+https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
+
+##Follow the following guide to install NeoPixel Libraries:##
+https://learn.adafruit.com/neopixels-on-raspberry-pi/python-usage
--- lights.py
+++ lights.py
@@ -3,6 +3,7 @@ import neopixel
 from time import sleep
 import collections
 
+
 def display(red, green, blue):
     print("Initializing lights")
     pixels = neopixel.NeoPixel(board.D18, 3)
@@ -20,36 +21,16 @@ def display(red, green, blue):
             pixels[x] = deq[x]
     sleep(5)
     pixels.deinit()
-    
-def main():
-    # time interval to travel across gradient
-    interval = 72
+
+
+def draw_colors(interval_start, interval_end, beginning_color,
+                ending_color, pixels):
+    interval = interval_end - interval_start
     interval_one = 1/interval
-
-    # Starting values for stage one
-    # rgb(70, 0, 120)
-    red_starting = 70
-    green_starting = 0
-    blue_starting = 120
-
-    # Ideally we shouldn't have to set these since these are the intermediary values,
-    #  but it helps keeps the colors going in the direction we want them to go
-    # rgb(178, 31, 31)
-    red_intermediate = 178
-    green_intermediate = 31
-    blue_intermediate = 31
-
-    # Final values for where the gradient should be. By the end of it we proabably
-    #  want to cycle back around to the beginning values.
-    # rgb(200, 60, 5)
-    red_final = 200
-    green_final = 60
-    blue_final = 5
-
     # The distance needed to go from one rgb value to another
-    red_delta = red_intermediate - red_starting
-    green_delta = green_intermediate - green_starting
-    blue_delta = blue_intermediate - blue_starting
+    red_delta = ending_color[0] - beginning_color[0]
+    green_delta = ending_color[1] - beginning_color[1]
+    blue_delta = ending_color[2] - beginning_color[2]
 
     # These factors just get us from one end of the gradient to the other
     #  in the given amount of time
@@ -57,63 +38,49 @@ def main():
     green_level_one_factor = green_delta*interval_one
     blue_level_one_factor = blue_delta*interval_one
 
-    print("Initializing lights")
-    pixels = neopixel.NeoPixel(board.D18, 144)
+    red = beginning_color[0]
+    green = beginning_color[1]
+    blue = beginning_color[2]
 
-    red = red_starting
-    green = green_starting
-    blue = blue_starting
-    print(int(red))
-    print(int(green))
-    print(int(blue))
     for x in range(interval):
-        red += red_level_one_factor
-        green += green_level_one_factor
-        blue += blue_level_one_factor
-        pixels[x] = ((int(red), int(green), int(blue)))
-
-    red_delta = red_final - red_intermediate
-    green_delta = green_final - green_intermediate
-    blue_delta = blue_final - blue_intermediate
-
-    red_level_one_factor = red_delta*interval_one
-    green_level_one_factor = green_delta*interval_one
-    blue_level_one_factor = blue_delta*interval_one
-
-    red = red_intermediate
-    green = green_intermediate
-    blue = blue_intermediate
-    print('red: ' + str(int(red)))
-    print('green: ' + str(int(green)))
-    print('blue: ' + str(int(blue)))
-    #~ sleep(10)
-    for x in range(interval):
-        print('interval + x = ' + str(interval+x))
-        if int(red) != red_final:
+        if int(red) != ending_color[0]:
             red += red_level_one_factor
-            pixels[interval + x] = ((int(red), int(green), int(blue)))
-        if int(green) != green_final:
+            pixels[x + interval_start] = ((int(red), int(green), int(blue)))
+        if int(green) != ending_color[1]:
             green += green_level_one_factor
-            pixels[interval + x] = ((int(red), int(green), int(blue)))
-        if int(blue) != blue_final:
+            pixels[x + interval_start] = ((int(red), int(green), int(blue)))
+        if int(blue) != ending_color[2]:
             blue += blue_level_one_factor
-            pixels[interval + x] = ((int(red), int(green), int(blue)))
-        
-        print('red: ' + str(int(red)))
-        print('green: ' + str(int(green)))
-        print('blue: ' + str(int(blue)))
-        if (int(red) == red_final and int(green) == green_final and int(blue) == blue_final):
-            print('set!')
+            pixels[x + interval_start] = ((int(red), int(green), int(blue)))
+        if (int(red) == ending_color[0] and int(green) == ending_color[1]
+                and int(blue) == ending_color[2]):
             break
         else:
             continue
+
+
+def main():
+    # rgb(70, 0, 120) - starting
+    # rgb(178, 31, 31) - intermediate
+    # rgb(200, 60, 5) - final
+    starting = (70, 0, 120)
+    intermediate = (178, 31, 31)
+    final = (200, 60, 5)
+    pixels = neopixel.NeoPixel(board.D18, 144)
+    print("Initialized lights")
+
+    draw_colors(0, 36, starting, intermediate, pixels)
+    draw_colors(36, 72, intermediate, final, pixels)
+    draw_colors(72, 108, final, intermediate, pixels)
+    draw_colors(108, 144, intermediate, starting, pixels)
     deq = collections.deque(pixels)
     while True:
-        print('rotating deque')        
+        print('rotating deque')
         deq.rotate()
         for x in range(len(pixels)-1):
             pixels[x] = deq[x]
-        
+
+
 if __name__ == '__main__':
-    #~ display(0, 0, 0)
+    # display(0, 0, 0)
     main()
